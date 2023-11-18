60aed34d8e737ed0d5fccf016f2111bfd8e2712b
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2023-11-18 12:30:54 +0800

    assigning keys to control turtle

--- kbgame.py
+++ kbgame.py
@@ -15,6 +15,23 @@ player.penup()
 # Set speed variable
 speed = 1
 
+# Define functions
+def turn_left():
+    player.left(30)
+
+def turn_right():
+    player.right(30)
+
+def increase_speed():
+    global speed
+    speed += 1
+
+# Set keyboard binding
+turtle.listen()
+turtle.onkey(turn_left, 'Left')
+turtle.onkey(turn_right, 'Right')
+turtle.onkey(increase_speed, 'Up')
+
 # Move the turtle
 while True:
     player.forward(speed)
