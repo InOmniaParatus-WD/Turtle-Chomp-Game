802c8693a49190bd60d2d32ec2a421781119cc42
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2023-11-18 14:38:21 +0800

    create opponent

--- kbgame.py
+++ kbgame.py
@@ -32,8 +32,23 @@ player.shape('turtle')
 player.penup()
 player.speed(0)
 
+# Create opponent turtle
+comp = turtle.Turtle()
+comp.color('red')
+comp.shape('turtle')
+comp.penup()
+comp.shapesize(2)
+comp.setposition(random.randint(-290, 290), random.randint(-290, 290))
+comp.speed(0)
+
+# Create competition score
+mypen2 = turtle.Turtle()
+mypen2.color('red')
+mypen2.hideturtle()
+
 # Create variable score
 score = 0
+comp_score = 0
 
 # Create food
 maxFoods = 10
@@ -93,6 +108,16 @@ turtle.onkey(decrease_speed, 'Down')
 
 while True:
     player.forward(speed)
+    comp.forward(12)
+    # Boundary Comp Checking x coordinate
+    if comp.xcor() > 290 or comp.xcor() < -290:
+        comp.right(180)
+        winsound.PlaySound('sounds/bounce.wav', winsound.SND_ASYNC)
+
+    # Boundary Comp Checking y coordinate
+    if comp.ycor() > 290 or comp.ycor() < -290:
+        comp.right(180)
+        winsound.PlaySound('sounds/bounce.wav', winsound.SND_ASYNC) 
 
     #Boundary Checking x cordinate
     if player.xcor() > 290 or player.xcor() <-290:
@@ -132,6 +157,21 @@ while True:
             mypen.setposition(-290, 310)
             scorestring ="Score: %s" % score
             mypen.write(scorestring, False, align='left', font=('Arial', 14, 'normal'))
+
+                # Comp Collision checking
+        if isCollision(comp, food):
+            food.setposition(random.randint(-290, 290), random.randint(-290, 290))
+            food.right(random.randint(0,360))
+            winsound.PlaySound('sounds/chomp.wav', winsound.SND_ASYNC)
+            comp_score+=1
+            # Draw the Comp score on the screen
+            mypen2.undo()
+            mypen2.penup()
+            mypen2.hideturtle()
+            mypen2.setposition(200, 305)
+            scorestring ="Score: %s" % comp_score
+            mypen2.write(scorestring, False, align='left', font=('Arial', 14, 'normal'))
+
 
 
 
